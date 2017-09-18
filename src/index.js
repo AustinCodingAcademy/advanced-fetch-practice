fac663b73f6f4789c31df4190ec9ee4bfe431d30
Ericka Emery <ericka@elonandcompany.com>
2017-09-18 00:49:30 -0500

    fetch practice complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-
+// fnc that is executed when data retrieved from the web.
 function usersRetrieved(response) {
   return response.json();
 }
+
+// next fnc handles when json data is ready
+// you control what you want to do with data here. manipulate, console log, etc
 function userJSONReady(users) {
   const usersDiv = document.getElementById("users");
   let usersHTML = "";
@@ -11,4 +14,36 @@ function userJSONReady(users) {
   }
   usersDiv.innerHTML = (usersHTML);
 }
+
+// decide what URL to use to call the data
+// call the fetch fnc to start web request that returns a promise
+// you decide what happens when web request is done. 
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then (usersRetrieved)
+.then(userJSONReady);
+
+
+// fnc that is executed when data retrieved from the web.
+function commentsRetrieved(response) {
+  return response.json();
+}
+
+// next fnc handles when json data is ready
+// you control what you want to do with data here. manipulate, console log, etc
+function commentJSONReady(comments) {
+  const commentsDiv = document.getElementById("comments");
+  let commentsHTML = "";
+  for (let i = 0; i < 10; i++) {
+    const comment = comments[i];
+    commentsHTML += "<div>" + comment.name + "</br>" + "</br>" + "</div>";
+  }
+  commentsDiv.innerHTML = (commentsHTML);
+}
+
+
+fetch("https://jsonplaceholder.typicode.com/comments")
+.then (commentsRetrieved)
+.then (commentJSONReady);
+
 
