79004c9975b02926df689611212ad22f6a5942a4
paulapbpb <paula@parcel22.com>
2017-10-30 20:33:51 -0500

    Add existing file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 function usersRetrieved(response) {
   return response.json();
 }
+
 function userJSONReady(users) {
   const usersDiv = document.getElementById("users");
   let usersHTML = "";
@@ -12,3 +13,20 @@ function userJSONReady(users) {
   usersDiv.innerHTML = (usersHTML);
 }
 
+function commentJsonReady(comments) {
+  const commentsDiv = document.getElementById("comments");
+  let commentsHTML = "";
+  for (let i = 0; i < comments.length; i++) {
+    const comment = comments[i];
+    commentsHTML += "<div>" + comment.name + "</div>";
+  }
+  commentsDiv.innerHTML = (commentsHTML);
+}
+
+const userswebRequestPromise = fetch("https://jsonplaceholder.typicode.com/users");
+const usersgetJsonPromise = userswebRequestPromise.then(usersRetrieved);
+usersgetJsonPromise.then(userJSONReady);
+
+const commentswebRequestPromise = fetch("https://jsonplaceholder.typicode.com/comments");
+const commentsgetJsonPromise = commentswebRequestPromise.then(usersRetrieved);
+commentsgetJsonPromise.then(commentJsonReady);
